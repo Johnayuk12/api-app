8f935242515b76cc2b28f921bda30de58588c1c8
johnayuk12 <john12ayuk@gmail.com>
2021-05-25 04:05:58 +0100

    changes

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,7 +15,6 @@ class PostResource extends JsonResource
     public function toArray($request)
     {
         return[
-            'id'            => (string)$this->id,
             'title' => $this->title,
             'description' => $this->description,
             'created_at' => (string) $this->created_at,
